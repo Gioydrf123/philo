890b0863775d8fbefd067c766fd3e13ff6c0418a
Gioydrf123 <giovannimuhey@gmail.com>
2025-06-28 13:36:00 +0200

    Update main.c
    aggiunta logica per eat

--- philosofers/src/main.c
+++ philosofers/src/main.c
@@ -77,22 +77,29 @@ bool create_philos(t_data_philo **data)
 
 void philo_eat(t_philo *philo_ptr)
 {
-    if ((data->id % 2) == 0)
-     {
-      pthread_mutex_lock(&philo->data->forks[right_fork]);
-      printf("Philosoper %d has taken a fork\n",data->id);
-      pthread_mutex_lock(&philo->data->forks[left_fork]);
-      printf("Philosoper %d has taken a fork\n", data->id);
-     }
-     else
-     {
-      pthread_mutex_lock(&philo->data->forks[left_fork]);
-      printf("Philosoper %d has taken a fork\n", data->id);
-      pthread_mutex_lock(&philo->data->forks[right_fork]);
-      printf("Philosoper %d has taken a fork\n",data->id);
-     }
-     printf("Philosoper %d is eating\n",data->id);
+	//da implementare un mutex per impedire race condition su stampa
+	int right_fork = (philo_ptr->id + 1) % philo_ptr->data->n_philo;
+	int left_fork = philo_ptr->id;
+
+	printf("tempo teorico 1: %d\n",philo_ptr->data->tt_die);
+	printf("tempo teorico 2: %d\n",philo_ptr->data->tt_eat);
+	if ((philo_ptr->id % 2) == 0)
+		swap_int_philo(&right_fork, &left_fork);
+	pthread_mutex_lock(&philo_ptr->data->forks[right_fork]);
+	printf("[%ld] Philosoper %d has taken a fork\n", get_timestamp(&philo_ptr->data->start_time), philo_ptr->id);
+	pthread_mutex_lock(&philo_ptr->data->forks[left_fork]);
+	printf("[%ld] Philosoper %d has taken a fork\n", get_timestamp(&philo_ptr->data->start_time), philo_ptr->id);
+	printf("[%ld] Philosoper %d is eating\n", get_timestamp(&philo_ptr->data->start_time), philo_ptr->id);
+	if(philo_ptr->data->tt_die - philo_ptr->data->tt_eat < 0)
+	{
+		usleep(philo_ptr->data->tt_die * 1000);
+		printf("[%ld] Philosoper %d is died\n", get_timestamp(&philo_ptr->data->start_time), philo_ptr->id);
+		philo_ptr->data->simulation_running = false;
+	}	
+	pthread_mutex_unlock(&philo_ptr->data->forks[right_fork]);
+	pthread_mutex_unlock(&philo_ptr->data->forks[left_fork]);
 }
+
 void simulation_running(t_philo *philo_ptr)
 {
   bool simulation_running = true;
