944be327a1aa16b5d0c75eb63acee93bdbb13922
Gioydrf123 <giovannimuhey@gmail.com>
2025-07-07 22:25:34 +0200

    Update main.c
    per ora il bonus e' cosi'

--- philo_bonus/src/main.c
+++ philo_bonus/src/main.c
@@ -1,175 +1,308 @@
-#include "../inc/philo.h"
+#include <fcntl.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-
-// long get_timestamp(struct timeval *t/ime_start)
-// {
-//   struct timeval now;
-//   gettimeofday(&now, NULL);
-//   return (now.tv_sec - start_time->tv_sec) * 1000 + (now.tv_usec - start_time->tv_usec) / 1000;
-// }
-
-int  free_p(t_data_philo **data, short ret)
+typedef struct s_philo
 {
-  if ((*data)->forks)
-  {
+    int        id;
+    int        n_philo;
+    int        tt_die;
+    int        tt_eat;
+    int        tt_sleep;
+    int        max_eat;
+    long    start_time;
+    sem_t    *forks_sem;
+    sem_t    *room_sem;
+    sem_t    *output_sem;
+    sem_t    *death_sem;
+}            t_philo;
+
+long    get_time_in_ms(void)
+{
+    struct timeval    tv;
+
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+}
+
+void    print_action(t_philo *philo, const char *msg)
+{
+    long    current_time;
+
+    sem_wait(philo->output_sem);
+    current_time = get_time_in_ms() - philo->start_time;
+    printf("%ld %d %s\n", current_time, philo->id, msg);
+    sem_post(philo->output_sem);
+}
+
+void    philo_exit(t_philo *philo)
+{
+    sem_wait(philo->death_sem);
+    sem_wait(philo->output_sem);
+    printf("%ld %d died\n", get_time_in_ms() - philo->start_time, philo->id);
+    exit(1);
+}
+
+void    philo_release(t_philo *philo)
+{
+    sem_post(philo->forks_sem);
+    sem_post(philo->forks_sem);
+    sem_post(philo->room_sem);
+}
+
+void    philosopher(t_philo *philo)
+{
+    long    last_eat;
+    int        meals;
+
+    last_eat = get_time_in_ms();
+    meals = 0;
+    while (1)
+    {
+        if (get_time_in_ms() - last_eat > philo->tt_die)
+            philo_exit(philo);
+        print_action(philo, "is thinking");
+        sem_wait(philo->room_sem);
+        sem_wait(philo->forks_sem);
+        print_action(philo, "has taken a fork");
+        if (philo->n_philo == 1)
+        {
+            usleep(philo->tt_die * 1000);
+            sem_post(philo->forks_sem);
+            sem_post(philo->room_sem);
+            philo_exit(philo);
+        }
+        sem_wait(philo->forks_sem);
+        print_action(philo, "has taken a fork");
+        last_eat = get_time_in_ms();
+        meals++;
+        print_action(philo, "is eating");
+        usleep(philo->tt_eat * 1000);
+        philo_release(philo);
+        if (philo->max_eat > 0 && meals >= philo->max_eat)
+            exit(0);
+        print_action(philo, "is sleeping");
+        usleep(philo->tt_sleep * 1000);
+    }
+}
+
+t_philo    initialize_philo(int id, long *data, sem_t *array[4])
+{
+    t_philo    philo = {.id = id, .n_philo = (int)data[0], .tt_die = (int)data[1],
+            .tt_eat = (int)data[2], .tt_sleep = (int)data[3],
+            .max_eat = (int)data[4], .start_time = data[5],
+            .forks_sem = array[0], .room_sem = array[1], .output_sem = array[2],
+            .death_sem = array[3]};
+
+    return (philo);
+}
+
+int    create_philosophers(long *data, sem_t *array[4], pid_t pids[])
+{
+    pid_t    pid;
+    t_philo    philo;
     int i;
-    i = (*data)->n_philo;
-    while(i-- > 0)
-      sem_destroy(&(*data)->forks);
-  }
-  // free((*data)->forks);
-  if ((*data)->philos)
-    free((*data)->philos);
-  if ((*data)->ids)
-    free((*data)->ids);
-  if ((*data))
-    free((*data));
-  return (ret);
+
+    i = 0;
+    while (i < data[0])
+    {
+        pid = fork();
+        if (pid == 0)
+        {
+            philo = initialize_philo(i, data, array);
+            philosopher(&philo);
+            exit(0);
+        }
+        else if (pid > 0)
+            pids[i] = pid;
+        else
+        {
+            perror("fork failed");
+            return (1);
+        }
+        i++;
+    }
+    return (0);
 }
 
-int create_sem_forks(t_data_philo **data)
+void    sem_un(void)
 {
-  int i;
-  i = 0;
-
-  // semaphores have an integer inside their structure
-
-  while (i < (*data)->n_philo)
-  {
-    // initializing array of semaphores
-    // TODO: add control for failing
-    sem_init(&(*data)->forks[i], 0, 1);
-		i++;
-  }
-  return (0);
+    sem_unlink("/forks");
+    sem_unlink("/room");
+    sem_unlink("/output");
+    sem_unlink("/death");
 }
 
-void parse_input(int argc, char **argv, t_data_philo **data)
+static int    ft_isdigit_philo(int c)
 {
-  *data = calloc(sizeof(t_data_philo), 1);
-  (*data)->n_philo = atoi(argv[1]);
-  (*data)->tt_die = atoi(argv[2]);
-  (*data)->tt_eat = atoi(argv[3]);
-  (*data)->tt_sleep = atoi(argv[4]);
-  if (argc == 6)
-    (*data)->max_eat = atoi(argv[5]);
-  else
-    (*data)->max_eat = -1;
+    if (!(c >= '0' && c <= '9'))
+        return (0);
+    return (1);
 }
 
-bool create_philos(t_data_philo **data)
+static long    ft_atol_philo(const char *str)
 {
-  int i;
-  i = 0;
-  //array di filosofi dentro data
-  (*data)->philos = calloc(sizeof(t_philo), (*data)->n_philo);
-  //ids sono gli ids
-  (*data)->ids = calloc(sizeof(pthread_t), (*data)->n_philo);
-  if (!(*data)->philos || !(*data)->ids)
-  {
-    return (true);
-  }
-  while(i < (*data)->n_philo)
-  {
-    (*data)->philos[i].id = i;
-    (*data)->philos[i].data = (*data); 
-    i++;
-  }
-  return (false);
+    long    value;
+
+    value = 0;
+    while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
+        str++;
+    while (*str)
+    {
+        if (!ft_isdigit_philo(*str))
+        {
+            printf("Error - only digits allowed\n");
+            exit(1);
+        }
+        value = value * 10 + (*str - '0');
+        if (value > 2147483647)
+            return (value);
+        str++;
+    }
+    return (value);
 }
 
-// sem_post : like mutex unlock
-
-void philo_eat(t_philo *philo_ptr)
+bool    check_min_max(int argc, char **argv)
 {
-    if ((data->id % 2) == 0)
-     {
-      sem_wait((*data)->forks[right_fork]);
-      sem_wait((*data)->fors[left_fork]);
-        //pthread_mutex_lock(&philo->data->forks[right_fork]);
-        //printf("Philosoper %d has taken a fork\n",data->id);
-        //pthread_mutex_lock(&philo->data->forks[left_fork]);
-        //printf("Philosoper %d has taken a fork\n", data->id);
-     }
-     else
-     {
-      sem_wait((*data)->forks[left_fork]);
-      sem_wait((*data)->fors[right_fork]);
-      // pthread_mutex_lock(&philo->data->forks[left_fork]);
-      // printf("Philosoper %d has taken a fork\n", data->id);
-      // pthread_mutex_lock(&philo->data->forks[right_fork]);
-      // printf("Philosoper %d has taken a fork\n",data->id);
-     }
-     printf("Philosoper %d is eating\n",data->id);
+    long    check_data[5];
+    int        i;
+
+    i = 0;
+    check_data[0] = ft_atol_philo(argv[1]);
+    check_data[1] = ft_atol_philo(argv[2]);
+    check_data[2] = ft_atol_philo(argv[3]);
+    check_data[3] = ft_atol_philo(argv[4]);
+    while (i < 4)
+        if (check_data[i] <= 0 || check_data[i++] > 2147483648)
+            return (printf("Error - max int or negative int\n"), true);
+    if (argc == 6)
+    {
+        check_data[4] = ft_atol_philo(argv[5]);
+        if (check_data[4] <= 0 || check_data[4] > 2147483648)
+            return (printf("Error - max in or negative int\n"), true);
+    }
+    return (false);
 }
 
-void simulation_running(t_philo *philo_ptr)
+void    parse_input(long *data, int argc, char **argv)
 {
-  bool simulation_running = true;
-
-  while(simulation_running)
-  {
-    // philo_eat(philo_ptr);
-    /*ATTENZIONE FUNZIONI DA IMPLEMENTARE LAVORARE SU QUESTE FUNZIONI SE POSSIBILE,
-    SI CAPISCE COSA FANNO 
-    philo_sleep();
-    philo_think();*/
-    i++;
-  }
+    if (check_min_max(argc, argv))
+        exit(1);
+    data[0] = ft_atol_philo(argv[1]);
+    data[1] = ft_atol_philo(argv[2]);
+    data[2] = ft_atol_philo(argv[3]);
+    data[3] = ft_atol_philo(argv[4]);
+    data[4] = -1;
+    if (argc == 6)
+        data[4] = ft_atol_philo(argv[5]);
 }
 
-void *philosophers_routine(void *philo_ptr)
+void    sem_op(sem_t *array[4], long data[6])
 {
-  t_philo *philo;
-  philo = (t_philo *)philo_ptr;
-  int left_fork = philo->id;
-  int right_fork = (philo->id + 1) % philo->data->n_philo;
-  // bool simulation_running= true;
-
-  printf("philo id: %d\n", philo->id);
-  printf("philo id: %d left fork: %d\n",philo->id, left_fork);
-  printf("philo id: %d pointer left fork: %p\n",philo->id, &philo->data->forks[left_fork]);
-  printf("philo id: %d right fork: %d\n",philo->id, right_fork);
-  printf("philo id: %d pointer right fork: %p\n",philo->id, &philo->data->forks[right_fork]);
-  printf("philo id: %d data address = %p\n", philo->id, &philo->data);
-
-
-  printf("filosofo id :%d\n", data->id);
-  simulation_running(philo);
-  printf("hello world");
+    // Handle room_sem for single philosopher
+    long room_sem_value = (data[0] == 1) ? 1 : data[0] - 1;
+    
+    array[0] = sem_open("/forks", O_CREAT, 0666, data[0]);
+    array[1] = sem_open("/room", O_CREAT, 0666, room_sem_value);
+    array[2] = sem_open("/output", O_CREAT, 0666, 1);
+    array[3] = sem_open("/death", O_CREAT, 0666, 1);
+    if (array[0] == SEM_FAILED || array[1] == SEM_FAILED
+        || array[2] == SEM_FAILED || array[3] == SEM_FAILED)
+    {
+        printf("sem_open failed\n");
+        exit(1);
+    }
 }
 
-int init_routine(t_data_philo **data)
+void    sem_cl(sem_t *array[4])
 {
-  int i;
-  i = 0;
-  while (i < (*data)->n_philo)
-  {
-    if (pthread_create(&(*data)->ids[i], NULL, &philosophers_routine, &(*data)->philos[i]))
-      return printf("error in thread creation\n");
-    usleep(1);
-    i++;
-  }
-  while (i-- > 0)
-		pthread_join((*data)->ids[i], NULL);
+    sem_close(array[0]);
+    sem_close(array[1]);
+    sem_close(array[2]);
+    sem_close(array[3]);
 }
 
-int main(int argc, char **argv)
+void    term(long *data, pid_t *pids)
 {
-  if (argc < 4 || argc > 5)
-    return (printf("error\n"));
-  t_data_philo *data = NULL;
-  parse_input(argc, argv, &data);
-  // updated create fork with semaphores
-  if (create_sem_forks(&data))
-    return (free_p(&data, 1));
+    int    status;
+    int    i;
+    int    j;
 
-  ///////////////////////////////
-  if (create_philos(&data))
-    return (free_p(&data, 1));
-  // printf("original data address: %p\n", data);
-  init_routine(&data);
+    i = 0;
+    while (i < (int)data[0])
+    {
+        waitpid(pids[i], &status, 0);
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 1)
+        {
+            j = 0;
+            while (j < (int)data[0])
+            {
+                if (j != i)
+                    kill(pids[j], SIGKILL);
+                j++;
+            }
+            return ;
+        }
+        i++;
+    }
+}
 
-  free_p(&data, 0);
-  return (0);
+static void    ft_bzero_variant(void *s, size_t n)
+{
+    char    *p;
+
+    p = s;
+    while (n--)
+        *p++ = '\0';
+}
+
+void    *ft_calloc_variant(size_t count, size_t size)
+{
+    void    *ptr;
+
+    ptr = malloc(size * count);
+    if (!ptr)
+    {
+        write(1, "Memory allocation error\n", 25);
+        free(ptr);
+        exit(1);
+    }
+    ft_bzero_variant(ptr, (count * size));
+    return (ptr);
+}
+
+int    main(int argc, char **argv)
+{
+    long    data[6];
+    sem_t    *array[4];
+    pid_t    *pids;
+
+    if (argc < 5 || argc > 6)
+    {
+        printf("Usage: %s number_of_philosophers", argv[0]);
+        return (printf(" time_to_die time_to_eat time_to_sleep [must_eat_times]\n"));
+    }
+    sem_un();
+    parse_input(data, argc, argv);
+    sem_op(array, data);
+    data[5] = get_time_in_ms();
+    pids = ft_calloc_variant(sizeof(pid_t), data[0]);
+    if (!pids)
+    {
+        printf("calloc error\n");
+        exit(1);
+    }
+    if (create_philosophers(data, array, pids) != 0)
+        return (1);
+    term(data, pids);
+    sem_cl(array);
+    sem_un();
+    free(pids);
+    return (0);
 }
